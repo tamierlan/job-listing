69b7ca84f781f79f242cb173f7074bb16be725ec
tamierlan <hudojnik.1993@gmail.com>
2020-12-26 17:06:02 -0500

    third-and-a-half commit

--- src/components/Job/NewJobModal.js
+++ src/components/Job/NewJobModal.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Box, Grid, FilledInput, Select, MenuItem, Dialog, DialogTitle, DialogContent, DialogAction } from '@material-ui/core';
+import { Button, makeStyles, Box, Grid, FilledInput, Select, MenuItem, Dialog, DialogTitle, DialogContent, DialogAction, Typography } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+  skillChip: {
+    margin: theme.spacing(0.5),
+    padding: theme.spacing(0.75),
+    fontSize: '14.5px',
+    borderRadius: '5px',
+    fontWeight: 600,
+    border: `1px solid ${theme.palette.secondary.main}`,
+    color: theme.palette.secondary.main,
+    cursor: 'pointer',
+
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.main,
+      color: '#fff'
+    }
+  }
+}))
 export default (props) => {
+  const classes = useStyles();
+  const skills = [
+    'JavaScript',
+    'React',
+    'Node',
+    'Vue',
+    'Firebase',
+    'MongoDB',
+    'SQL'
+  ]
   return (
     <Dialog open={true} fullWidth>
       <DialogTitle>Post Jobs</DialogTitle>
       <DialogContent>
         <Grid spacing={2} container>
+
           <Grid item xs={6}>
             <FilledInput fullWidth placeholder='Job title' disableUnderline />
           </Grid>
+
           <Grid item xs={6}>
             <Select fullWidth variant='filled' disableUnderline defaultValue='Full time'>
               <MenuItem value='Full time'>Full time</MenuItem>
@@ -17,26 +46,46 @@ export default (props) => {
               <MenuItem value='Contact'>Contact</MenuItem>
             </Select>
           </Grid>
+
           <Grid item xs={6}>
             <FilledInput fullWidth placeholder='Company name' disableUnderline />
           </Grid>
+
           <Grid item xs={6}>
             <FilledInput fullWidth placeholder='Company URL' disableUnderline />
           </Grid>
+
           <Grid item xs={6}>
             <Select fullWidth variant='filled' disableUnderline defaultValue='Remote'>
               <MenuItem value='Remote'>Remote</MenuItem>
               <MenuItem value='In-office'>In-office</MenuItem>
             </Select>
           </Grid>
+
           <Grid item xs={6}>
             <FilledInput fullWidth placeholder='Job Link' disableUnderline />
           </Grid>
+
           <Grid item xs={12}>
             <FilledInput fullWidth placeholder='Job description' disableUnderline multiline rows={3} />
           </Grid>
+
         </Grid>
+        <Box mt={2}>
+          <Typography>Skills</Typography>
+          <Box display='flex'>
+            {skills.map(skill => (
+              <Box className={classes.skillChip} key={skill}>{skill}</Box>
+            ))}
+          </Box>
+        </Box>
       </DialogContent>
+      <DialogAction>
+        <Box>
+          <Typography>Required fields</Typography>
+          <Button></Button>
+        </Box>
+      </DialogAction>
     </Dialog>
   )
 }
