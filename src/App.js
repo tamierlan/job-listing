cbd239c156dc56c1a08bfe0935766106cc7c7a7d
tamierlan <hudojnik.1993@gmail.com>
2020-12-26 14:11:46 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import { Box, ThemeProvider, Grid } from "@material-ui/core";
+import theme from './theme/theme';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import JobCard from './components/Job/JobCard';
 
 export default () => {
-  return <Box>Desi Codes</Box>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Grid container justify='center'>
+        <Grid item xs={10}>
+          <SearchBar />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+          <JobCard />
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  )
 };
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Button, Select, MenuItem, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  wrapper: {
+    backgroundColor: '#fff',
+    display: 'flex',
+    boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.1)',
+    borderRadius: '5px',
+    '& > *': {
+      flex: 1,
+      height: '45px',
+      margin: '8px'
+    },
+  },
+})
+
+export default (props) => {
+  const classes = useStyles()
+  return (
+    <Box p={2} mt={-5} mb={2} className={classes.wrapper}>
+      <Select variant='filled' disableUnderline defaultValue='Full time'>
+        <MenuItem value='Full time'>Full time</MenuItem>
+        <MenuItem value='Part time'>Part time</MenuItem>
+        <MenuItem value='Contact'>Contact</MenuItem>
+      </Select>
+      <Select variant='filled' disableUnderline defaultValue='Remote'>
+        <MenuItem value='Remote'>Remote</MenuItem>
+        <MenuItem value='In-office'>In-office</MenuItem>
+      </Select>
+      <Button variant='contained' color='primary' disableElevation>
+        Search
+      </Button>
+    </Box>
+  )
+}
