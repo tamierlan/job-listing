68cf49a3a788c3bbabc82cd1c0d42e1beaa431ec
tamierlan <hudojnik.1993@gmail.com>
2020-12-28 11:20:16 -0500

    build added

---
+++ build/precache-manifest.cd954fac3c33d60c22e6c0cca2f3d4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859722c8232aff39dd35a670f83f1525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f50ec5c709fff0108e54",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ca7ff9277ec7a368c71",
+    "url": "/static/js/2.00e32997.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.00e32997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50ec5c709fff0108e54",
+    "url": "/static/js/main.b01ef4c6.chunk.js"
+  },
+  {
+    "revision": "bf9fda22f05c083606a6",
+    "url": "/static/js/runtime-main.ccf115f7.js"
+  }
+]);
